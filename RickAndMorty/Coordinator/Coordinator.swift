8732985023d9dec115c5bace45daad392564d674
anntumdev <a.tumashova.dev@gmail.com>
2024-11-21 21:30:36 +0200

    Добавлен Coordinator

---
+++ RickAndMorty/Coordinator/Coordinator.swift
@@ -0,0 +1,29 @@
+//
+//  Coordinator.swift
+//  RickAndMorty
+//
+//  Created by Anna on 21.11.2024.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var finishDelegate: CoordinatorFinishDelegate? { get }
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+    var dependencies: IDependencies { get }
+    func start()
+    func finish()
+}
+
+extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinator: Coordinator)
+}
