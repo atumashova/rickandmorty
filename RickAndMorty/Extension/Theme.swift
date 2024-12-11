f362c5873c266c7acc8f2e7662d82ec35eed288e
atumashova <anechka596@mail.ru>
2024-12-11 23:04:30 +0200

    Убрано принудительное извлечение (шрифты и цвета)

--- RickAndMorty/Extension/Theme.swift
+++ RickAndMorty/Extension/Theme.swift
@@ -16,13 +16,13 @@ extension UIFont {
 }
 
 struct ColorTheme {
-    let secondBackground = UIColor(named: "SecondBackground")!
-    let blueBackground = UIColor(named: "BlueBackground")!
-    let blueText = UIColor(named: "BlueText")!
+    let secondBackground = UIColor(named: "SecondBackground") ?? UIColor.gray
+    let blueBackground = UIColor(named: "BlueBackground") ?? UIColor.blue
+    let blueText = UIColor(named: "BlueText") ?? UIColor.blue
 }
 
 struct FontTheme {
-    let filter = UIFont(name: "Roboto-Medium", size: 14)!
-    let episode = UIFont(name: "Roboto-Regular", size: 16)
-    let character = UIFont(name: "Roboto-Medium", size: 20)
+    let filter = UIFont(name: "Roboto-Medium", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .medium)
+    let episode = UIFont(name: "Roboto-Regular", size: 16) ?? UIFont.systemFont(ofSize: 16)
+    let character = UIFont(name: "Roboto-Medium", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .medium)
 }
