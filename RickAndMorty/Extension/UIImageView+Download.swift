9e4880b0eb94afd07bd341cfe95169735d061239
atumashova <anechka596@mail.ru>
2024-12-11 22:48:46 +0200

    Обработка ошибок при скачивании фото

--- RickAndMorty/Extension/UIImageView+Download.swift
+++ RickAndMorty/Extension/UIImageView+Download.swift
@@ -7,26 +7,60 @@
 
 import Foundation
 import UIKit
+// Типы ошибок при скачивании фото
+enum ImageDownloadError: Error {
+    case invalidURL
+    case networkError(Error)
+    case badResponse
+    case invalidMimeType(String)
+    case imageDataCreationError
+    case unknown
+}
 
 extension UIImageView {
-    
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit, completion: @escaping (Result<UIImage, ImageDownloadError>) -> Void) {
         contentMode = mode
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
+            if let error = error {
+                completion(.failure(.networkError(error)))
+                return
+            }
+            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else {
+                completion(.failure(.badResponse))
+                return
+            }
+            guard let mimeType = response?.mimeType, mimeType.hasPrefix("image") else {
+                completion(.failure(.invalidMimeType(response?.mimeType ?? "неизвестно")))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.imageDataCreationError))
+                return
+            }
+            guard let image = UIImage(data: data) else {
+                completion(.failure(.imageDataCreationError))
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(image))
             }
         }.resume()
     }
     
     func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+        downloaded(from: url, contentMode: mode) { [weak self] result in
+            switch result {
+            case .success(let image):
+                self?.image = image
+            case .failure(let failure):
+                DispatchQueue.main.async {
+                    self?.tintColor = UIColor.black.withAlphaComponent(0.6)
+                    self?.contentMode = .center
+                    let configuration = UIImage.SymbolConfiguration(pointSize: 60)
+                    self?.image = UIImage(systemName: "xmark.circle")?.withConfiguration(configuration)
+                }
+            }
+        }
     }
 }
